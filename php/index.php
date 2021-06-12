1ff2a832de6c57c81b877bd836e27d95b95cd714
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-06-12 16:22:59 -0500

    feat: Seccion carousel inicial

--- php/index.php
+++ php/index.php
@@ -25,9 +25,45 @@ require_once './includes/navbar.php';
     </div>
 </div>
 
-<main class="principal">
+<main class="container principal mb-5">
+    <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block " src="../img/partidos/pan.png" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Partido Accion nacional</h5>
+                    <a href="articulos.php" type="button" class="btn  bg-boton">Ver articulos</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block " src="../img/partidos/pan.png" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Partido Accion nacional</h5>
+                    <a href="articulos.php" type="button" class="btn  bg-boton">Ver articulos</a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block " src="../img/partidos/pan.png" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <a href="articulos.php" type="button" class="btn  bg-boton">Ver articulos</a>
 
-
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente</span>
+        </a>
+    </div>
 </main>
 
 <?php
