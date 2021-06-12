f60d3a33fd86b38ae903bee4a14fe23a53132d2d
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-06-12 18:43:33 -0500

    fix: Solucionando rutas de importaciones

--- php/includes/header.php
+++ php/includes/header.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html>
+<?php
+$root = "http://localhost/dipu-net";
+?>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Dipu-net</title>
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="<?php echo $root . "/css/bootstrap.min.css"; ?>">
+    <link rel="stylesheet" href="<?php echo $root . "/css/styles.css"; ?>">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
 </head>
 
