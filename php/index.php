4aad072cb892aed5dc75892a7b89bf57c22a82b5
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-06-12 15:32:24 -0500

    feat: seccion Acerca de

--- php/index.php
+++ php/index.php
@@ -2,6 +2,29 @@
 require_once './includes/navbar.php';
 ?>
 
+<div class="banner mt-5">
+    <p>
+        Candidatos y candidatas a diputados
+        <br>
+        ¡Conocelos!
+    </p>
+
+</div>
+
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 my-auto">
+            <h2>Acerca de la página</h2>
+            <p>Dipunet es una plataforma donde usted puede consultar los arituclos correspondientes a cada uno de los candidatos a diputados del Estado de México.</p>
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-12 ml-auto">
+            <figure>
+                <img src="../img/vote.png" alt="" width="100%">
+            </figure>
+        </div>
+    </div>
+</div>
+
 <main class="principal">
 
 
