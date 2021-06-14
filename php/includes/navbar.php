cba2189f7fc31912d59b3046cefd5e330c543691
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-06-14 09:22:05 -0500

    feat: Mostrando correo del usuario logueado, habilitando logout

--- php/includes/navbar.php
+++ php/includes/navbar.php
@@ -1,5 +1,10 @@
 <?php
-require_once (realpath(dirname(__DIR__) . '/includes/header.php'));
+require_once(realpath(dirname(__DIR__) . '/includes/header.php'));
+session_start();
+if ($_SERVER['QUERY_STRING'] == 'logout') {
+    session_unset();
+    header('Location: index.php');
+}
 ?>
 <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-principal ">
     <div class="container">
@@ -20,12 +25,29 @@ require_once (realpath(dirname(__DIR__) . '/includes/header.php'));
                 </li>
             </ul>
             <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link " href="login.php">iniciar sesión</a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link " href="register.php">registrarse</a>
-                </li>
+                <?php
+                if (isset($_SESSION['usuario']['correo'])) {
+                ?>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link " href="mi-cuenta.php"><?php echo $_SESSION['usuario']['correo']; ?></a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link " href="index.php?logout">Cerrar sesión</a>
+                    </li>
+                <?php
+                } else {
+                ?>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link " href="login.php">iniciar sesión</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link " href="register.php">registrarse</a>
+                    </li>
+                <?php
+                }
+                ?>
+
+
             </ul>
         </div>
     </div>
