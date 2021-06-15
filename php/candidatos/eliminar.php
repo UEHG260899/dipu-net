4e1c661624ed5fc7af9761d9627935df299bf59a
alexgoga120 <alexgoga120@gmail.com>
2021-06-15 16:24:39 -0500

    Se corrigieron lineas innecesarias

--- php/candidatos/eliminar.php
+++ php/candidatos/eliminar.php
@@ -1,5 +1,5 @@
 <?php
-require_once(realpath(dirname(__FILE__) . "../../../includes/navbar_escritor.php"));
+require_once(realpath(dirname(__FILE__) . "../../../includes/navbar_admin.php"));
 ?>
 
 <main>
