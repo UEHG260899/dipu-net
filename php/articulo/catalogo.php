6d1a45855864910ca96f1b98aa60aca49d18eb81
Cristhian Rivera <riverarodriguezcristhian@hotmail.com>
2021-06-15 15:13:42 -0500

    Diseño en botones de modal

--- php/articulo/catalogo.php
+++ php/articulo/catalogo.php
@@ -143,18 +143,18 @@ require_once '../includes/navbar.php';
     </div>
 </div>
 
-<!-- The Modal -->
+
 <div class="modal fade" id="mdlPartidos">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
 
-      <!-- Modal Header -->
+      
       <div class="modal-header">
         <h4 class="modal-title">Seleccione un Partido Político o Coalición</h4>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
 
-      <!-- Modal body -->
+    
       <div class="modal-body">
         <div class="row mb-3">
             <div class="col-md-6">
@@ -272,9 +272,10 @@ require_once '../includes/navbar.php';
 
       </div>
 
-      <!-- Modal footer -->
+
       <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn bg-boton" data-dismiss="modal"><span><i class="fa fa-check-circle"></i></span> Elegir</button>
+        <button type="button" class="btn btn-info" data-dismiss="modal"><span><i class="fa fa-times-circle"></i></span> Cancelar</button>
       </div>
 
     </div>
