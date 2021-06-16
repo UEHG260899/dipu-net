b3ee2d13db2a4dba5e2ebb0f38ae0808af643dba
CarlosSerranoV <carlosjsv251999@gmail.com>
2021-06-15 23:59:33 -0500

    feat: Se agrego funcionalidad a pagina crear articulo y validaciones

--- js/escritor/crear.js
+++ js/escritor/crear.js
@@ -1,3 +1,40 @@
-$( document ).ready(function() {
-    $('#mostrar').slideToggle();
-});
+$(document).ready(function () {
+
+    $("#guardar").click(function () {
+  
+      $('#opcion').val("guardar");
+      $('#idC').val($('#idCandidato').val());
+  
+      var valResult = validarCampos($('.validar'));
+      if (valResult.success) {
+          if($('#idC').val().length!=0){
+             $("#formAgregar").submit(); 
+          }else{
+              $('#modal').modal('show');
+          }
+          
+      }
+  
+    });
+  
+    $("#publicar").click(function () {
+  
+      $('#opcion').val("publicar");
+      $('#idC').val($('#idCandidato').val());
+  
+      var valResult = validarCampos($('.validar'));
+      if (valResult.success) {
+          if($('#idC').val().length!=0){
+             $("#formAgregar").submit(); 
+          }else{
+              $('#modal').modal('show');
+          }
+          
+      }
+    });
+  
+    $("#articulo").keyup(function(){
+      $("#preArticulo").val($("#articulo").val());
+    });
+  });
+  
