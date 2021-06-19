45dd37880d98eaa971c956b2149ee6628ff2eb3c
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-06-19 13:21:51 -0500

    feat: Mostrar Total de articulos guardados por lector, mostrar total de comentarios por lector

--- php/mi-cuenta.php
+++ php/mi-cuenta.php
@@ -28,11 +28,23 @@ if (isset($_SESSION['usuario'])) {
     } else {
         echo "Algo ha salido mal al momento de realizar la consulta";
     }
+    /**
+     * ? Obtener total de articulos guardados por el lector actual
+     */
+    $sqlGuardados = "SELECT COUNT(*) as guardados FROM mas_tarde  WHERE id_lector =" . $_SESSION['id_lector'] . " ";
+    $guardados = mysqli_query($conn, $sqlGuardados);
+    $nGuardados = mysqli_fetch_assoc($guardados);
+    /**
+     * ? Obtener total de articulos comentados por el lector actual
+     */
+    $sqlComentados = "SELECT COUNT(*) as comentados FROM comentarios  WHERE id_lector =" . $_SESSION['id_lector'] . " ";
+    $comentados = mysqli_query($conn, $sqlComentados);
+    $nComentados = mysqli_fetch_assoc($comentados);
 }
 ?>
-<div class="container mt-5">
-    <h1>Mi cuenta</h1>
-    <div class="card">
+<div class="container mt-3">
+    <h1 class="animate__animated animate__zoomIn">Mi cuenta</h1>
+    <div class="card animate__animated animate__zoomIn">
 
         <div class="card-body">
             <div class="row">
@@ -55,7 +67,7 @@ if (isset($_SESSION['usuario'])) {
             </div>
             <div class="row">
                 <div class="offset-lg-1 col-lg-6 col-md-6 col-sm-8 ">
-                    <button class="btn bg-boton">Editar mis datos</button>
+                    <a href=<?php echo $root . '/php/lectores/editar.php' ?> class="btn bg-boton">Editar mis datos</a>
                 </div>
             </div>
 
@@ -63,7 +75,7 @@ if (isset($_SESSION['usuario'])) {
 
         </div>
     </div>
-    <div class="card mt-5">
+    <div class="card mt-5 animate__animated animate__fadeInUp">
 
         <div class="card-body">
             <div class="row">
@@ -75,9 +87,9 @@ if (isset($_SESSION['usuario'])) {
                                     Artículos Guardados
                                 </div>
                                 <div class="card-body">
-                                    <h5 class="card-title text-center"><strong>5</strong></h5>
+                                    <h5 class="card-title text-center"><strong><?php echo $nGuardados['guardados']; ?></strong></h5>
                                     <div class="d-flex justify-content-center">
-                                        <button class="btn bg-boton mt-3">Ver todo</button>
+                                        <a href="lectores/guardados.php" class="btn bg-boton mt-3">Ver todo</a>
                                     </div>
                                 </div>
                             </div>
@@ -88,7 +100,7 @@ if (isset($_SESSION['usuario'])) {
                                     Articulos comentados
                                 </div>
                                 <div class="card-body">
-                                    <h5 class="card-title text-center"><strong>7</strong></h5>
+                                    <h5 class="card-title text-center"><strong><?php echo $nComentados['comentados']; ?></strong></h5>
                                     <div class="d-flex justify-content-center">
                                         <button class="btn bg-boton mt-3">Ver todo</button>
                                     </div>
